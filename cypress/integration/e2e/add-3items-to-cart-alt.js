0fa6e0d98342ba0c5e586c881be3d27531d3b5d1
rula-banat <rolabanat@gmail.com>
2023-06-20 08:16:43 +0300

    rename folder

--- cypress/integration/e2e/add-3items-to-cart-alt.js
+++ cypress/integration/e2e/add-3items-to-cart-alt.js
@@ -1,9 +1,10 @@
-describe('Add to cart', () => {
-    it('Add 3 items to cart', () => {
+describe('Add to cart alternatively', () => {
+    it('Add 3 items to cart alternatively', () => {
      cy.visit("https://www.saucedemo.com/")
      cy.get('[data-test="username"]').type("standard_user")
      cy.get('[data-test="password"]').type("secret_sauce")
      cy.get('[data-test="login-button"]').click()
+     //Add item and skip the following one
      for (let i = 0; i < 6; i++){
         if (i % 2 == 0){
         cy.get('div#inventory_container').find('.btn').eq(i).click()
